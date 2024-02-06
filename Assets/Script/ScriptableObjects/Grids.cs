265f8aa5d205109e2b87dbf2e0416a0bd73aa108
yichenglu-ylu7 <125488002+yichenglu-ylu7@users.noreply.github.com>
2024-02-06 11:16:29 -0500

    Create gird ScriptableObject

---
+++ Assets/Script/ScriptableObjects/Grids.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GridData", menuName = "ScriptableObjects/GridScriptableObject", order = 1)]
+public class GridScriptableObject : ScriptableObject
+{
+    public Vector2 currentCoordinate;
+}
