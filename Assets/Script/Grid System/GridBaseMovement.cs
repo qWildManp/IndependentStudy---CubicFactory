580884829a1aefd6325562f497a81083f402ccd3
yichenglu-ylu7 <125488002+yichenglu-ylu7@users.noreply.github.com>
2024-02-06 11:47:28 -0500

    Test grid base movement
    Add grid base movement for test grid system

---
+++ Assets/Script/Grid System/GridBaseMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBaseMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Transform movePoint;
+
+    private void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position,movePoint.position) <= 0.05f)
+        {
+            if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+            }
+            
+            if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                movePoint.position += new Vector3(0f, 0f, Input.GetAxisRaw("Vertical"));
+            }
+        }
+    }
+}
