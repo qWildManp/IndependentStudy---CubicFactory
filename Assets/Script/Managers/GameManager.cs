21613c2370e768a9c744b3157ea5a02274fe2710
qWildManp <harryttt@126.com>
2024-04-21 17:45:33 -0400

    Add sounder manager; adding In game Pause Panel

--- Assets/Script/Managers/GameManager.cs
+++ Assets/Script/Managers/GameManager.cs
@@ -12,19 +12,31 @@ public class GameManager : MonoSingleton<GameManager>
     [SerializeField] private Box playerAttachBox;
     
     [SerializeField] public CamRootRotation camRoot;
+    [SerializeField] public bool gamePause;
     
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        
+        if(Input.GetKeyDown(KeyCode.Escape))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            if (gamePause)
+            {
+                EventBus.Broadcast(EventTypes.PauseGame,false);
+                SoundManager.Instance.PlayBtnClick();
+                UIManager.Instance.Resume();
+            }
+            else
+            {
+                EventBus.Broadcast(EventTypes.PauseGame,true);
+                SoundManager.Instance.PlayBtnClick();
+                UIManager.Instance.ShowPausePanel();
+            }
         }
     }
 
@@ -33,6 +45,7 @@ public class GameManager : MonoSingleton<GameManager>
         base.Init();
         EventBus.AddListener(EventTypes.ClearPlayerInteractBox,ClearPlayerAttachBox);
         EventBus.AddListener<Box>(EventTypes.RemovePlayerInteractingBox,RemoveInteractingBox);
+        EventBus.AddListener<bool>(EventTypes.PauseGame,PauseGame);
         GridSystem.Instance.InitializeGrid();
     }
 
@@ -40,6 +53,7 @@ public class GameManager : MonoSingleton<GameManager>
     {
         EventBus.RemoveListener(EventTypes.ClearPlayerInteractBox,ClearPlayerAttachBox);
         EventBus.RemoveListener<Box>(EventTypes.RemovePlayerInteractingBox,RemoveInteractingBox);
+        EventBus.RemoveListener<bool>(EventTypes.PauseGame,PauseGame);
     }
 
     public bool SetPlayerAttachBox(Box interactBox)// set player attached Box
@@ -52,6 +66,23 @@ public class GameManager : MonoSingleton<GameManager>
         {
             this.playerAttachBox = interactBox;
             return true;
+        }
+        
+    }
+
+    private void PauseGame(bool result)
+    {
+        if (result)
+        {
+            Time.timeScale = 0;
+            gamePause = true;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            gamePause = false;
+            Cursor.visible = false;
         }
         
     }
@@ -75,5 +106,22 @@ public class GameManager : MonoSingleton<GameManager>
     public Box GetPlayerAttachedBox()// Get player attached Box
     {
         return playerAttachBox;
+    }
+
+    public void ReTryLevel()
+    {
+        StartCoroutine(DelayLoadLevel(SceneManager.GetActiveScene().name));
+    }
+
+    public void BackToMain()
+    {
+        StartCoroutine(DelayLoadLevel("StartScene"));
+    }
+    
+    IEnumerator DelayLoadLevel(string LevelName)
+    {
+        yield return new WaitForSecondsRealtime(0.15f);
+        Time.timeScale = 1;
+        SceneManager.LoadScene(LevelName);
     }
 }
