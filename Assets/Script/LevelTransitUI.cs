b6e4a41b1d70f030abac4e3548003e1bdcbcc873
Jason <qiuj1@rose-hulman.edu>
2024-04-30 16:37:29 -0400

    Added level title

---
+++ Assets/Script/LevelTransitUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTransitUI : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+    public float fadeSpeed = 1;
+    void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        StartCoroutine(FadeOut());
+    }
+
+    private IEnumerator FadeOut()
+    {
+        float e = 1;
+        while (e > 0)
+        {
+            canvasGroup.alpha = e;
+            e -= fadeSpeed * Time.deltaTime;
+            yield return null;
+        }
+    }
+}
